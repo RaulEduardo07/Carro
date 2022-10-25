e680033a7da805eda8d8d2936d9988747bd96d38
raul_ <raul_nunes07@hotmail.com>
2022-10-24 22:33:33 -0300

    Initial commit

---
+++ src/Carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+
+        String cor;
+
+
+
+    String placa;
+    public Carro() {
+    }
+
+    public Carro(String cor, String placa) {
+        this.cor = cor;
+        this.placa = placa;
+    }
+}
+
